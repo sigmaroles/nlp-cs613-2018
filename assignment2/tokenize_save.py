0b96457430d8a3f9bf4c7c07ac65354b8ef3cc8e
sigmaroles <sohhom@gmail.com>
2018-09-16 19:22:57 +0530

    non random train test split

--- assignment2/tokenize_save.py
+++ assignment2/tokenize_save.py
@@ -1,14 +1,12 @@
 from nltk.tokenize import sent_tokenize
 import string
-import numpy as np
-#from ngram_util import NGramModel
-
 with open('alice_wonderland.txt', 'r', encoding='utf-8') as fh:
     alltext = fh.read()
 
 cleaned_text = alltext.lower()
 cleaned_text = cleaned_text.translate({ord('\n'):' ', 
-                                  ord('\"'):' ', 
+                                  ord('\"'):' ',
+                                  ord('”'):' ', 
                                   ord('’'):' ',
                                   ord('‘'):' ',    
                                   ord('\''):' ',
@@ -21,22 +19,25 @@ sents = sent_tokenize(cleaned_text)
 translator = str.maketrans(string.punctuation, ' '*len(string.punctuation))
 sents = [x for x in map(lambda x: x.translate(translator).strip(), sents)]
 sents = [x for x in map(lambda x: x + ' </s>', sents)]
-sents = np.array(sents)
+# it's ugly, but basically the next line filters out the extra spaces between words (introduced in preprocessing steps)
+sents = [[fword for fword in filter(lambda x: len(x)>0, [wrd.strip() for wrd in sent.split(' ')])] for sent in sents]
 
+# sanity check
+print (len(sents))
+print (sents[45])
 
 with open('corpus_ALL.txt', 'w', encoding='utf-8') as fh:
-    for x in sents:
-        fh.write(x.strip()+' ')
-
-train_sents = np.random.choice(sents, size=int(0.8 * len(sents)), replace=False)
-test_sents = np.random.choice(sents, size=int(0.2 * len(sents)), replace=False)
-
+    for sent in sents:
+        for word in sent:
+            fh.write(word+' ')
+            
+train_sents = sents[:int(.8*len(sents))]
+test_sents = sents[(int(.2*len(sents)))]
 with open('corpus_train.txt', 'w', encoding='utf-8') as fh:
-    for x in filter(lambda x: len(x)>0, [x.split() for x in train_sents]):
+    for x in train_sents:
         for word in x:
-            fh.write(word.strip()+' ')
-
+            fh.write(word+' ')
 with open('corpus_test.txt', 'w', encoding='utf-8') as fh:
-    for x in filter(lambda x: len(x)>0, [x.split() for x in test_sents]):
+    for x in test_sents:
         for word in x:
-            fh.write(word.strip()+' ')
+            fh.write(word+' ')
