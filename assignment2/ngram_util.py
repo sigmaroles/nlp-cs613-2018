819ff2acdcca10878b3dde406d55a09a4d2adbc9
sigmaroles <sohhom@gmail.com>
2018-09-17 12:16:16 +0530

    get_proba for add one finalized. Sentence generation stable(for add one)

--- assignment2/ngram_util.py
+++ assignment2/ngram_util.py
@@ -13,27 +13,29 @@ class NGramModel():
         self.subModel = None
         self.smoothing = smoothing
         self.vocab = set(corpus)
-        
-        
         if smoothing not in ['addone', 'goodturing', None]:
             raise ValueError('Unknown smoothing type : {}'.format(smoothing))
-        #print ("Initialized {}-gram model with vocab size {}. Please call .buildModels() before doing anything else.".format(n, len(self.vocab)))
+        
         
     def buildModels(self):
         # iterate over corpus and enumerate/store ngrams
-        
         for i in range(self.corpus_length - (self.n-1)):
             this_tuple = tuple([self.corpus[tindx] for tindx in range(i, i+self.n)])
             self.ngrams.append(this_tuple)
         
         self.counter = Counter(self.ngrams)
-        self.count_matrix = {} # dict of dicts
-        
-        if self.n>1:
+        if self.n>1: # bigram and higher
             # for probabilities of ngram model, we will also need counts of n-1 gram
             self.subModel = NGramModel(self.n-1, self.corpus)
             self.subModel.buildModels()
 
+            self.known_ngram_proba = {}
+            for cc in self.counter:
+                g_phrase = cc[:-1]
+                n_word = cc[-1]
+                if g_phrase not in self.known_ngram_proba:
+                    self.known_ngram_proba[g_phrase] = {}
+                self.known_ngram_proba[g_phrase][n_word] = self._get_proba(cc)
     
     def get_count(self, ngram):
         if not type(ngram) == tuple:
@@ -55,12 +57,45 @@ class NGramModel():
         else: #good turing
             pass
                 
-    def generate_sentence(self, max_length=30):
+    def generate_sentence(self, max_length=30, start_words = None):
+        if start_words:
+            start_words = tuple(start_words.split(' '))
+            if not (len(start_words) == self.n-1):
+                raise ValueError("Needed {} start word(s), got {}".format(self.n-1, len(start_words)))
+        else:  # find a random phrase to start with
+            idx = np.random.choice(len(self.ngrams),1)[0]
+            start_words = self.ngrams[idx][:-1]
         
-        start_word = self.ngrams
+        sentence = [x for x in start_words]
+        if self.n>1:
+            n = self.n - 1
+            while True:
+                start_words = tuple(sentence[-n:])
+                known_completions = self.known_ngram_proba[start_words]
+                words = np.array([x for x in known_completions.keys()])
+                probas = np.array([x for x in known_completions.values()])
+                probas /= probas.sum() # potentially wrong thing to do
+                new_word = np.random.choice(words, 1, replace=False, p=probas)
+                sentence.extend(new_word)
+                max_length -= 1
+                if max_length <= 0 or new_word == '</s>':
+                    break
+        else: # unigram, simplest probability sampling
+            all_proba = np.array([self._get_proba(x) for x in self.ngrams])
+            all_proba /= all_proba.sum()
+            words = np.array(self.ngrams).reshape(-1,)
+            while True:
+                new_word = np.random.choice(words, replace=False, p=all_proba)
+                sentence.append(new_word)
+                max_length -= 1
+                if max_length <= 0 or new_word == '</s>':
+                    break
+                
+        return ' '.join(sentence)
+        
 
 
-    def get_proba(self, ngram):
+    def _get_proba(self, ngram, logspace=False):
         if not type(ngram) == tuple:
             ngram = tuple(ngram.split(' '))
         ln = len(ngram)
@@ -74,3 +109,5 @@ class NGramModel():
             this_count = self.get_count(ngram)
             return this_count / self.subModel.get_count(ngram[:-1])
 
+    def get_probability(self, sentence):
+        pass
--- assignment2/scratchpad.py
+++ assignment2/scratchpad.py
@@ -6,11 +6,18 @@ with open('corpus_ALL.txt', 'r', encoding='utf-8') as fh:
     
 corpus = tt.split(' ')
 
-#m1 = NGramModel(2,corpus, smoothing='addone')
-m1 = NGramModel(2,corpus)
+m1 = NGramModel(2,corpus, smoothing='addone')
+#m1 = NGramModel(2,corpus)
 m1.buildModels()
 
 
 #for word in ['thought', 'mouse', 'alice', 'dormouse']:
-for word in ['said alice', 'alice thought']:
-    print (word, m1.get_count(word), m1.get_proba(word))
+for word in ['said alice', 'thought she']:
+    print (word, m1.get_count(word), m1._get_proba(word))
+
+for i in range(20):
+    #print (m1.generate_sentence(start_words='said'))
+    print (m1.generate_sentence())
+
+
+#m1.generate_sentence()
