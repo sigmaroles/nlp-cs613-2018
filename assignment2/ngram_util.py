bd33acdad0ed02d5cdf34477aa0d7ef7e2c0fb28
sigmaroles <sohhom@gmail.com>
2018-09-17 18:38:24 +0530

    good turing complete, including constant discounting plot

--- assignment2/ngram_util.py
+++ assignment2/ngram_util.py
@@ -9,15 +9,16 @@ class NGramModel():
         self.counter = None
         self.corpus_length = len(corpus)
         self.ngrams = []
-        self.count_matrix = None
         self.subModel = None
         self.smoothing = smoothing
         self.vocab = set(corpus)
+        self.count_of_count = None
         if smoothing not in ['addone', 'goodturing', None]:
             raise ValueError('Unknown smoothing type : {}'.format(smoothing))
         
         
     def buildModels(self):
+        print ("Inside build models of ",self.n,"gram")
         # iterate over corpus and enumerate/store ngrams
         for i in range(self.corpus_length - (self.n-1)):
             this_tuple = tuple([self.corpus[tindx] for tindx in range(i, i+self.n)])
@@ -26,17 +27,20 @@ class NGramModel():
         self.counter = Counter(self.ngrams)
         if self.n>1: # bigram and higher
             # for probabilities of ngram model, we will also need counts of n-1 gram
-            self.subModel = NGramModel(self.n-1, self.corpus)
+            self.subModel = NGramModel(self.n-1, self.corpus, smoothing=self.smoothing)
             self.subModel.buildModels()
+            if self.smoothing == 'goodturing':
+                self.count_of_count = Counter(self.counter.values())
 
-            self.known_ngram_proba = {}
+            self.known_ngram_proba  = {}
             for cc in self.counter:
                 g_phrase = cc[:-1]
                 n_word = cc[-1]
                 if g_phrase not in self.known_ngram_proba:
                     self.known_ngram_proba[g_phrase] = {}
                 self.known_ngram_proba[g_phrase][n_word] = self._get_proba(cc)
-    
+
+            
     def get_count(self, ngram):
         if not type(ngram) == tuple:
             ngram = tuple(ngram.split(' '))
@@ -44,19 +48,55 @@ class NGramModel():
         if not ln == self.n:
             raise ValueError("Provided n-gram is of length {}, where it should have been {}".format(ln,self.n))
         
-        
-        this_count = self.counter[ngram] if self.counter[ngram] else 0
-        if ln==1: return this_count
+        actual_count = self.counter[ngram] if self.counter[ngram] else 0
+        if self.n == 1: #unigram, nothing else to calculate
+            return actual_count
         
         if not self.smoothing:
-            return this_count
+            return actual_count
         elif self.smoothing=='addone':
             g_phrase = ngram[:-1]
             submodel_count = self.subModel.get_count(g_phrase)
-            return (this_count + 1) / (submodel_count + len(self.vocab)) * submodel_count
-        else: #good turing
-            pass
+            return (actual_count + 1) / (submodel_count + len(self.vocab)) * submodel_count
+        else: #good turing smoothing
+            n_c_plus_one = self.count_of_count[actual_count+1]
+            n_c = self.count_of_count[actual_count]
+            adjusted_count = (actual_count + 1) * (n_c_plus_one / n_c)
+            return adjusted_count
+
                 
+    def _get_proba(self, ngram, logspace=False):
+        if not type(ngram) == tuple:
+            ngram = tuple(ngram.split(' '))
+        ln = len(ngram)
+        if not ln == self.n:
+            raise ValueError("Provided n-gram is of length {}, where it should have been {}".format(ln,self.n))
+
+        if ln==1: #unigram special case
+            return self.get_count(ngram) / len(self.corpus)
+        else:
+            actual_count = self.counter[ngram] if self.counter[ngram] else 0
+            submodel_count = self.subModel.get_count(ngram[:-1])
+            if not self.smoothing:
+                return actual_count / submodel_count
+            elif self.smoothing=='addone':
+                return (actual_count + 1 ) / (submodel_count + len(self.vocab))
+            else: # good turing
+                N = len(self.ngrams)
+                if not actual_count: # zero count of given ngram
+                    return self.count_of_count[1] / N
+                else:
+                    n_c = self.count_of_count[actual_count]
+                    n_c_plus_one = self.count_of_count[actual_count+1]
+                    adjusted_count = (actual_count + 1) * (n_c_plus_one / n_c)
+                    return (adjusted_count / N)
+
+
+
+    def get_probability(self, sentence):
+        pass
+
+
     def generate_sentence(self, max_length=30, start_words = None):
         if start_words:
             start_words = tuple(start_words.split(' '))
@@ -92,22 +132,12 @@ class NGramModel():
                     break
                 
         return ' '.join(sentence)
+
+    def get_d_list(self):
+        ngrams_smallcounts = list(filter(lambda x: self.counter[x]<=10, self.counter))
+        ngram_smallcounts_goodturing = [self.get_count(x) for x in ngrams_smallcounts]
+        ngram_smallcounts_original = [self.counter[x] for x in ngrams_smallcounts]
+        ngrams_both = list(zip(ngram_smallcounts_goodturing, ngram_smallcounts_original))
+        d_values = list(map(lambda x: x[1]-x[0], ngrams_both))
+        return d_values
         
-
-
-    def _get_proba(self, ngram, logspace=False):
-        if not type(ngram) == tuple:
-            ngram = tuple(ngram.split(' '))
-        ln = len(ngram)
-        if not ln == self.n:
-            raise ValueError("Provided n-gram is of length {}, where it should have been {}".format(ln,self.n))
-
-
-        if ln==1: #unigram special case
-            return self.get_count(ngram) / len(self.corpus)
-        else:
-            this_count = self.get_count(ngram)
-            return this_count / self.subModel.get_count(ngram[:-1])
-
-    def get_probability(self, sentence):
-        pass
